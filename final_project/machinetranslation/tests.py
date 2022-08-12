f78e26604346be0e111c2df46656f9e618b50f5b
willm625 <wcornej1@binghamton.edu>
2022-08-12 16:41:58 +0000

    fixed test
    Signed-off-by: willm625 <wcornej1@binghamton.edu>

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,11 +2,24 @@ import unittest
 
 from translator import french_to_english, english_to_french
 
-class TestfrenchtoEnglish(unittest.TestCase):
+class TestTranslator(unittest.TestCase):
     def test1(self):
+        print('frenchToEgnlish - assertEqual')
         self.assertEqual(None, french_to_english(None))
-        self.assertEqual(None, english_to_french(None))
-    def test2(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+    def test2(self):
+        print('frenchToEnglish - assertNotEqual')
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bye')
+        self.assertNotEqual(french_to_english(None), 'Hello')
+        
+    def test3(self):
+        print('englishToFrench - assertEqual')
+        self.assertEqual(None, english_to_french(None))
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        
+    def test4(self):
+        print('englishToFrench - assertNotEqual')
+        self.assertNotEqual(english_to_french('Hello'), 'Goodbye')
+        self.assertNotEqual(english_to_french(None), 'Hello')
+        
 unittest.main()        
